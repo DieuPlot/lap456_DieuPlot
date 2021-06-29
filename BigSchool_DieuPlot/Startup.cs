9a3a98c34b00145804e07822c27ed676f9e121e8
Windows 10 <Windows 10@DESKTOP-0CUDA44>
2021-06-29 15:38:43 +0700

    Add project files.

---
+++ BigSchool_DieuPlot/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool_DieuPlot.Startup))]
+namespace BigSchool_DieuPlot
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
